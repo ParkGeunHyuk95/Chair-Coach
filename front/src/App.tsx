9bdeafc430f82e33f917840d0116d752a9859754
parkgeunhyuk1 <108388408+parkgeunhyuk1@users.noreply.github.com>
2023-01-03 17:21:11 +0900

    [Fix]: prevent build error

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -30,6 +30,13 @@ import userState from "./atoms/user";
 import WindowSizeError from "./components/windowSizeError/WindowSizeError";
 
 function App() {
+  const handleTokenUndefined = async () => {
+    let refreshToken = await localStorage.getItem("refreshToken");
+    let accessToken = await sessionStorage.getItem("accessToken");
+    if (refreshToken === "undefined" && accessToken === "undefined") {
+      setUser(null);
+    }
+  };
   const setUser = useSetRecoilState(userState);
   const user = useRecoilValue(userState);
   const handleStorageChange = async function () {
@@ -44,7 +51,6 @@ function App() {
   // 아래의 fetchCurrentUser 함수가 실행된 다음에 컴포넌트가 구현되도록 함.
   // 아래 코드를 보면 isFetchCompleted 가 true여야 컴포넌트가 구현됨.
   const [isFetchCompleted, setIsFetchCompleted] = useState(false);
-
   const fetchCurrentUser = async () => {
     try {
       // 이전에 발급받은 토큰이 있다면, 이를 가지고 유저 정보를 받아옴.
@@ -63,6 +69,10 @@ function App() {
   if (!isFetchCompleted) {
     handleStorageChange();
   }
+
+  useEffect(() => {
+    handleTokenUndefined();
+  }, []);
   return (
     <RecoilRoot>
       <BrowserRouter>
